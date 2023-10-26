3fe793431bfe04672798fa1d19a92956e9e6db58
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-10-26 22:33:19 +0400

    feat(): read user input for the message

--- sender.go
+++ sender.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
+var senderName = "YourName"
+
+func sendMessageToServer(serverName string, db *sql.DB, message string) {
+	// TODO
+}
+
+func main() {
+	// TODO make the port dynamic
+	databaseConnections := InitializeDatabaseConnections()
+
+	var message string
+	fmt.Print("Enter a message: ")
+	_, err := fmt.Scan(&message)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
